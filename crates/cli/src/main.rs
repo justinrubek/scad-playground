e331d186a79ddabbd1678b4722e9105371adac3d
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2025-03-01 19:11:20 -0600

    feat: closed gap

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -17,9 +17,13 @@ fn main() -> Result<()> {
             let cmd = generate.command;
             match cmd {
                 GenerateCommands::Default => {
-                    let cube = cube!([25., 35., 55.], true);
+                    let cube_a = cube!([60., 20., 10.], true);
+                    let mut cube_b = cube!([30., 20., 10.], true);
+                    cube_b = translate!([0., 0., 10.], cube_b;);
+                    let mut connector = cube!([30., 20., 0.002], true);
+                    connector = translate!([0., 0., 5.-0.001], connector;);
 
-                    let finished = cube;
+                    let finished = cube_a + cube_b + connector;
                     finished.save("default.scad");
                 }
             }
