4c4ac9513ae9f72751ea4b727b8bdb8bbf54d78b
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2025-03-01 19:12:56 -0600

    feat: car body

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -8,6 +8,16 @@ use scad_tree::prelude::*;
 mod commands;
 mod error;
 
+fn car_body() -> Scad {
+    let cube_a = cube!([60., 20., 10.], true);
+    let mut cube_b = cube!([30., 20., 10.], true);
+    cube_b = translate!([0., 0., 10.], cube_b;);
+    let mut connector = cube!([30., 20., 0.002], true);
+    connector = translate!([0., 0., 5.-0.001], connector;);
+
+    return cube_a + cube_b + connector;
+}
+
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
@@ -17,13 +27,9 @@ fn main() -> Result<()> {
             let cmd = generate.command;
             match cmd {
                 GenerateCommands::Default => {
-                    let cube_a = cube!([60., 20., 10.], true);
-                    let mut cube_b = cube!([30., 20., 10.], true);
-                    cube_b = translate!([0., 0., 10.], cube_b;);
-                    let mut connector = cube!([30., 20., 0.002], true);
-                    connector = translate!([0., 0., 5.-0.001], connector;);
+                    let body = car_body();
 
-                    let finished = cube_a + cube_b + connector;
+                    let finished = body;
                     finished.save("default.scad");
                 }
             }
